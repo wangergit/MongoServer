6c246cbba0cccb2ef90cabf2ce42ef445cc06743
liyang <286820894@qq.com>
2019-05-29 09:18:38 +0800

    add

---
+++ src/com/mongodb/controller/Hello.java
@@ -0,0 +1,50 @@
+package com.mongodb.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.dao.Point;
+import com.mongodb.service.MongoDaoImpl;
+
+import java.util.List;
+
+@Controller
+public class Hello 
+{
+	@Autowired
+	private MongoDaoImpl mongoDao;	
+    @RequestMapping("/hello")
+    public String hello(Model model)
+    {
+        model.addAttribute("msg","hello springmvc!");
+        return "index";
+    }
+    @RequestMapping("/near")
+    public String near(Model model)
+    {
+    	DBObject query = new BasicDBObject();
+    	Point point = new Point();
+    	point.setLng(-73.871150);
+    	point.setLat(40.6730975);
+    	int limit = 5;
+    	Long maxDistance = 5000L; // รื
+    	List<DBObject> list = mongoDao.geoNear("restaurants", query, point, limit, maxDistance);
+    	for(DBObject obj : list)
+    	   System.out.println(obj);
+
+        model.addAttribute("results",list);
+        return "index";
+    }
+    @RequestMapping("/count")
+    public String count(Model model)
+    {
+    	DBObject query = new BasicDBObject();
+    	Long count = mongoDao.count("restaurants", query);
+    	
+        model.addAttribute("msg",count);
+        return "index";
+    }
+}
